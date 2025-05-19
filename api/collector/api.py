e59c81bd70da48b34fedf590249505720e2af90a
Xandy <alexandrearthurpg@gmail.com>
2025-05-19 19:12:54 -0300

    feat: add documentation the routers of collector

--- api/collector/api.py
+++ api/collector/api.py
@@ -9,13 +9,19 @@ def index():
     return {"Data": "Hello Word"}
 
 
-@router.post("/colect_data")
+@router.post(
+    "/colect_data",
+    description="Rota para realizar a busca de ID's de artigos através das Keywords pré-setadas e salvar em CSV no BackEnd",
+)
 def start_collect_article_id(start: int = 0, limit: int = 1000, step: int = 1000):
     core.collect_articleID(start=start, limit=limit, step=step)
     return {"Data": "Iniciando Processamento"}
 
 
-@router.post("/colect_abstract")
-def start_collect_abastract(start: int = 0, limit: int = 0, max_threads: int = 10):
+@router.post(
+    "/colect_abstract",
+    description="Rota para fazer a coleta do resumo dos artigos coletados pela Coleta e salvar-los no CSV no BackEnd",
+)
+def start_collect_abstract(start: int = 0, limit: int = 0, max_threads: int = 10):
     core.collect_abstract(start=start, limit=limit, max_threads=max_threads)
     return {"Data": "Colentando Abstract dos Documentos"}
